7eb0d4c9cd8c882d0ce4c60cb59e7a4bd68de8ce
BHRnR <benni.hacker@gmail.com>
2020-09-24 17:19:24 +0200

    Add wavy light

--- Windfury.ino
+++ Windfury.ino
@@ -2,52 +2,79 @@ const int buttonPin = 2;
 const int redLedPin = 10;
 const int blueLedPin = 5;
 
-int timer = 0;
+int pseudoRandomNumber = 0;
 int buttonState = 0;
 int lastButtonState = 0;
+int brightness = 0;
 
+int proccEveryXPress = 1;
+int wavyDuration = 7;
 int blinkTimes = 3;
-int blinkDuration = 200;
-int onAfterBlink = 1000;
-int blinkEveryXPress = 5;
 
 void setup() {
   pinMode(buttonPin, INPUT);
   pinMode(redLedPin, OUTPUT);
   pinMode(blueLedPin, OUTPUT);
-  Serial.begin(9600); // initialize serial communication
+  Serial.begin(9600);
 }
 
 void waitingState() {
-  digitalWrite(redLedPin, LOW);
-  digitalWrite(blueLedPin, HIGH);
+  analogWrite(redLedPin, 0);
+  analogWrite(blueLedPin, 255);
+  if (brightness < 20) {
+    while (brightness < 255) {
+      dimmBrighter();
+    }
+  } else {
+    dimmDarker();
+  }
 }
 
-bool randomGenerator() {
-  return timer % blinkEveryXPress == 0;
+void dimmDarker () {
+  analogWrite(blueLedPin, brightness);
+  delay(wavyDuration);
+  brightness--;
+}
+
+void dimmBrighter () {
+  analogWrite(blueLedPin, brightness);
+  delay(wavyDuration);
+  brightness++;
+}
+
+bool proccChecker() {
+  return pseudoRandomNumber % proccEveryXPress == 0;
 }
 
 void blinkRed() {
+  brightness = 0;
   for (int i = 0; i < blinkTimes; i++) {
-    digitalWrite(redLedPin, HIGH);
-    delay(blinkDuration);
-    digitalWrite(redLedPin, LOW);
-    delay(blinkDuration);
+    while (brightness > 0) {
+      analogWrite(redLedPin, brightness);
+      brightness--;
+      delay(1);
+    }
+    while (brightness < 255) {
+      analogWrite(redLedPin, brightness);
+      brightness++;
+      delay(1);
+    }
   }
-  digitalWrite(redLedPin, HIGH);
-  delay(onAfterBlink);
-  digitalWrite(redLedPin, LOW);
+  while (brightness > 0) {
+    analogWrite(redLedPin, brightness);
+    brightness--;
+    delay(5);
+  }
 }
 
 void loop() {
   waitingState();
   buttonState = digitalRead(buttonPin);
-  if (buttonState != lastButtonState && buttonState == HIGH && randomGenerator()) {
-    digitalWrite(blueLedPin, LOW);
+  if (buttonState != lastButtonState && buttonState == HIGH && proccChecker()) {
+    analogWrite(blueLedPin, 0);
     blinkRed();
-    digitalWrite(blueLedPin, HIGH);
     delay(50);
   }
   lastButtonState = buttonState;
-  timer++;
+  pseudoRandomNumber++;
 }
